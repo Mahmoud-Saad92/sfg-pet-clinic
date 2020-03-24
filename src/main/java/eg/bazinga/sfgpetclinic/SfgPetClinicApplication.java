3f7f90c16ef8aa3c2c115a1a111387962695e27c
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-03-24 19:11:15 +0200

    Initial Project Add

---
+++ src/main/java/eg/bazinga/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package eg.bazinga.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
