9a33d04ad540a317770935a087f6aeadcc97d0fe
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-03-30 18:29:57 +0200

    List All Vets on Vet Index Page. Closes #14

--- pet-clinic-web/src/main/java/eg/bazinga/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/eg/bazinga/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,26 @@
 package eg.bazinga.sfgpetclinic.controllers;
 
+import eg.bazinga.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private VetService vetServiceMap;
+
+    @Autowired
+    public VetController(VetService vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetServiceMap.findAll());
 
         return "vets/index";
     }
