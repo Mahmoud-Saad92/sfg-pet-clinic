8294da9513ac7389e73a9700a758fb1b1c79faae
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-04-06 14:40:32 +0200

    Convert Vets to JPA Entity. Closes #35

--- pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/Vet.java
@@ -1,10 +1,17 @@
 package eg.bazinga.sfgpetclinic.models;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "VETS")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vets_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities;
 
     public Set<Speciality> getSpecialities() {
