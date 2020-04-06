21fce5c848893b8628d511d61b1d983cede4cdf7
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-04-06 15:42:56 +0200

    Spring Data JPA Service for Vets Specialty. Closes #42

---
+++ pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/services/jpa/SpecialityJpaService.java
@@ -0,0 +1,51 @@
+package eg.bazinga.sfgpetclinic.services.jpa;
+
+import eg.bazinga.sfgpetclinic.models.Speciality;
+import eg.bazinga.sfgpetclinic.repositories.SpecialityRepository;
+import eg.bazinga.sfgpetclinic.services.SpecialityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("jpa")
+public class SpecialityJpaService implements SpecialityService {
+
+    private SpecialityRepository specialityRepository;
+
+    @Autowired
+    public SpecialityJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().iterator().forEachRemaining(specialities::add);
+
+        return specialities;
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
