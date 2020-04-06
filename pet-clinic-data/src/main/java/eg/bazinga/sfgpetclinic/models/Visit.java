7d7c0a666da82e28415577a420e0186f9b308d35
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-04-06 14:47:46 +0200

    Create Visit Entity. Closes #36

--- pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/Visit.java
+++ pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/Visit.java
@@ -1,11 +1,23 @@
 package eg.bazinga.sfgpetclinic.models;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "VISITS")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
