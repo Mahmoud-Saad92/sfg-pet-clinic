a432d39c7035424ab62261e2d8070c124850de93
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-04-06 14:28:28 +0200

    Convert Owner to JPA Entity. Closes #34

--- pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/Pet.java
+++ pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/Pet.java
@@ -2,15 +2,27 @@ package eg.bazinga.sfgpetclinic.models;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "PETS")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
-    private PetType petType;
-    private Owner owner;
+
+    @Column(name = "birth_day")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
 
     public String getName() {
         return name;
--- pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/PetType.java
+++ pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/PetType.java
@@ -1,7 +1,14 @@
 package eg.bazinga.sfgpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PET_TYPE")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
