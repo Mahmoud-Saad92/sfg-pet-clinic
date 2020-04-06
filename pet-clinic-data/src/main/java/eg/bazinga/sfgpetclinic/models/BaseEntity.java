d70ca268ebe23ea680ffa0d2628755ff6f886e0f
Mahmoud.Saad <Mahmoud.Saad@asset.com.eg>
2020-04-06 14:23:53 +0200

    Revert "Menu Links are Broken. Closes #43"
    This reverts commit bc54faca

--- pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/eg/bazinga/sfgpetclinic/models/BaseEntity.java
@@ -1,16 +1,9 @@
 package eg.bazinga.sfgpetclinic.models;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-@MappedSuperclass
 public class BaseEntity implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
